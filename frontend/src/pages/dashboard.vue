0268ef90cb078295c92142c7ac6eaa56e297b3fd
Thomas Thomsen <tth@keepfocus.dk>
2023-03-29 15:10:55 +0200

    country component

--- frontend/src/pages/dashboard.vue
+++ frontend/src/pages/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="dashboard flex bg-purple-50">
+    <div class="dashboard flex bg-purple-50 h-auto min-h-screen">
        <navigation />
-        <div class="px-12 pt-8 w-[calc(100vw-24rem)]">
+        <div class="ml-[23rem] px-12 pt-8 w-[calc(100vw-24rem)]">
             <router-view></router-view>
         </div>
    
