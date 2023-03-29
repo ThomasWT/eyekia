563ad7752113bc63ec0b093c5b000b17cef3a1ed
Thomas Thomsen <tth@keepfocus.dk>
2023-03-29 16:12:11 +0200

    fixes

--- frontend/src/components/onlineorders.vue
+++ frontend/src/components/onlineorders.vue
@@ -8,7 +8,7 @@
             <caption class="px-5 py-2 text-lg font-semibold text-left text-gray-900 bg-white">
                 <p class="mt-1 text-sm font-normal text-gray-500 ">Recent orders from both online and in store sales</p>
             </caption>
-            <thead class="text-xs text-gray-700 uppercase bg-purple-200 ">
+            <thead class="text-xs text-gray-600 uppercase bg-purple-200 ">
                 <tr>
                     <th scope="col" class="px-6 py-3">
                         Product
@@ -29,7 +29,7 @@
             </thead>
             <tbody>
                 <tr v-for="order in orders" :key="order.productname" class="bg-white border-b">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    <th scope="row" class="px-6 py-4 font-bold text-gray-500 whitespace-nowrap">
                         {{ order.productname }}
                     </th>
                     <td class="px-6 py-4">
@@ -42,7 +42,7 @@
                         €{{ order.price }}
                     </td>
                     <td class="pr-3 py-4 text-right">
-                        <span :class="{'bg-green-100 text-green-600' : order.state == 'completed', 'bg-yellow-100 text-yellow-600' : order.state == 'awaiting', 'bg-red-100 text-red-500' : order.state == 'cancelled'}" class="text-sm font-medium px-2.5 py-0.5 rounded capitalize">{{ order.state }}</span>
+                        <span :class="{'bg-green-100 text-green-600' : order.state == 'completed', 'bg-yellow-100 text-yellow-600' : order.state == 'awaiting', 'bg-red-100 text-red-500' : order.state == 'cancelled'}" class="text-sm font-medium px-2.5 py-0.5 rounded capitalize shadow-sm">{{ order.state }}</span>
                     </td>
                 </tr>
                
