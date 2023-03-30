00982f4cb8b5c2d286ee12608347c87f324d77ad
Thomas Thomsen <twt@outlook.dk>
2023-03-30 12:04:39 +0200

    darkmode to media

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,7 @@
 // tailwind.config.js
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  media: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
