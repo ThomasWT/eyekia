6f58107211244a5408fd8f24b7fafdf739fea42b
Thomas Thomsen <twt@outlook.dk>
2023-03-30 21:12:02 +0200

    router animation

--- frontend/src/pages/dashboard.vue
+++ frontend/src/pages/dashboard.vue
@@ -2,7 +2,11 @@
     <div class="dashboard flex bg-purple-50 h-auto min-h-screen">
        <navigation />
         <div class="ml-[25rem] px-12 pt-6 w-[calc(100vw-27rem)] flex flex-wrap">
-            <router-view class=" flex flex-wrap w-full flex-col"></router-view>
+            <router-view class=" flex flex-wrap w-full flex-col"  v-slot="{ Component }">
+                <transition name="fade">
+    <component :is="Component" />
+  </transition>
+            </router-view>
         </div>
     </div>
 </template>
@@ -18,3 +22,15 @@ export default defineComponent({
     }
 })
 </script>
+
+<style lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
